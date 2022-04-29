97295cedb09e9f346d59e45cc6ef34d7ad6a7dad
Jeff Mendez <jeff@a11ywatch.com>
2022-04-29 19:51:41 -0400

    release: v0.0.2 (#1)

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -2,8 +2,6 @@ Scan() {
    a11ywatch scan --url "${PARAM_TO}" -s
 }
 
-# Will not run if sourced for bats-core tests.
-# View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
     Scan
